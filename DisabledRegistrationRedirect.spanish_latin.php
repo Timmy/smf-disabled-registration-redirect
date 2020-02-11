0db49b3fe260a193488a08467eae940f80a43017
Timmy <1987229+Timmy@users.noreply.github.com>
2020-02-11 20:51:12 +0100

    Add Latin Spanish translation

---
+++ DisabledRegistrationRedirect.spanish_latin.php
@@ -0,0 +1,7 @@
+<?php
+/* Spanish translation https://www.bombercode.net Copyright 2014-2019 */
+
+$txt['disabled_registration_redirect_title'] = 'Redireccionamiento de registro deshabilitado';
+$txt['disabled_registration_redirect_url'] = 'URL de redireccionamiento de registro deshabilitado';
+$txt['disabled_registration_redirect_url_info'] = 'Solo aplica si el proceso de registro est&aacute; deshabilitado';
+$txt['disabled_registration_redirect_url_example'] = 'Por ejemplo: https://www.bombercode.net';
