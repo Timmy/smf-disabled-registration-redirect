28dfaa6c84ea747ad615968d1040ee6b9c694515
Timmy <1987229+Timmy@users.noreply.github.com>
2020-02-11 20:48:08 +0100

    Add Russian translation

---
+++ DisabledRegistrationRedirect.russian.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['disabled_registration_redirect_title'] = 'Перенаправление при отключенной регистрации';
+$txt['disabled_registration_redirect_url'] = $txt['disabled_registration_redirect_title'];
+$txt['disabled_registration_redirect_url_info'] = 'Применяется только при отключенной регистрации';
+$txt['disabled_registration_redirect_url_example'] = 'например, http://example.com';
