2c87e9156bd7e5cd134b574d47cce4736e50fdc7
Timmy <1987229+Timmy@users.noreply.github.com>
2020-02-11 20:46:45 +0100

    Add Dutch translation

---
+++ DisabledRegistrationRedirect.dutch.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['disabled_registration_redirect_title'] = 'Omleiden bij uitgeschakelde registratie';
+$txt['disabled_registration_redirect_url'] = 'URL-omleiding bij uitgeschakelde registratie';
+$txt['disabled_registration_redirect_url_info'] = 'Alleen van toepassing wanneer registratie is uitgeschakeld';
+$txt['disabled_registration_redirect_url_example'] = 'bijv. http://example.com';
